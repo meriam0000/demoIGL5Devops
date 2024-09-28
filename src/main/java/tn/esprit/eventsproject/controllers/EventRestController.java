71fc67109795d7b4899d9d3b786f9aff9a453037
LENOVO <merriambelhadjhamida@gmail.com>
2024-09-28 12:11:22 +0200

    changed endpoint name

--- src/main/java/tn/esprit/eventsproject/controllers/EventRestController.java
+++ src/main/java/tn/esprit/eventsproject/controllers/EventRestController.java
@@ -13,7 +13,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RequiredArgsConstructor
-@RequestMapping("event")
+@RequestMapping("events")
 @RestController
 public class EventRestController {
     private final IEventServices eventServices;
